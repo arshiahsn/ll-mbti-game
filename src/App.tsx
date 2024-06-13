ac982f5f042f25c8fbca1731f764ff1c17d7f03e
Arshia Hsn <ahosseini@lodgelink.com>
2024-06-13 16:24:36 -0600

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -23,10 +23,19 @@ interface Guess {
 }
 
 const predefinedPlayers: Player[] = [
-  { id: 0, name: "Alice" },
-  { id: 1, name: "Bob" },
-  { id: 2, name: "Charlie" },
-  { id: 3, name: "Diana" },
+  { id: 0, name: "Arshia" },
+  { id: 1, name: "Nupur" },
+  { id: 2, name: "Rodrigo" },
+  { id: 3, name: "Raina" },
+  { id: 4, name: "Shahrzad" },
+  { id: 5, name: "Findlay" },
+  { id: 6, name: "Sloane" },
+  { id: 7, name: "Yash" },
+  { id: 8, name: "Adebayo" },
+  { id: 9, name: "Stewart" },
+  { id: 10, name: "Jason" },
+  { id: 11, name: "Irfan" },
+  { id: 12, name: "Vaibhav" },
 ];
 
 function App() {
@@ -34,34 +43,27 @@ function App() {
   const [isDone, setIsDone] = useState(false);
   const [hasPlayed, setHasPlayed] = useState(false);
 
-  const playerGuessExists = (
-    guessData: any,
-    email: string
-  ) => {
-    const playerGuess =
-      guessData.find((guess: any) => {
-        const { id } = guess ?? {};
-        return id === email
-      });
+  const playerGuessExists = (guessData: any, email: string) => {
+    const playerGuess = guessData.find((guess: any) => {
+      const { id } = guess ?? {};
+      return id === email;
+    });
 
     return !!playerGuess;
   };
 
   const fetchUserEmail = async () => {
-    const { email = '' } = await fetchUserAttributes();
+    const { email = "" } = await fetchUserAttributes();
     return email;
-  }
-
-  const fetchGuesses = async () => {
-    const { data: items } = await client.models.Guesses.list();
-    const email = await fetchUserEmail();
-    const exists = playerGuessExists(items, email) ?? {};
-    setHasPlayed(exists);
   };
 
 
 
-  const submitGuesses = async (newGuesses: Guess, selectedPlayer: Player, loginId: string) => {
+  const submitGuesses = async (
+    newGuesses: Guess,
+    selectedPlayer: Player,
+    loginId: string
+  ) => {
     try {
       await client.models.Guesses.create({
         id: loginId,
@@ -75,6 +77,14 @@ function App() {
   };
 
   useEffect(() => {
+    const fetchGuesses = async () => {
+      const { data: items } = await client.models.Guesses.list();
+      const email = await fetchUserEmail();
+      console.log(email, items);
+      const exists = playerGuessExists(items, email);
+      console.log("exists", exists);
+      setHasPlayed(exists);
+    };
     fetchGuesses();
   }, []);
 
@@ -92,7 +102,7 @@ function App() {
               />
             ) : (
               <>
-                {(!isDone || hasPlayed) &&(
+                {(!isDone || hasPlayed) && (
                   <>
                     <h2>
                       Welcome, {selectedPlayer.name} ({selectedPlayer.mbti})
@@ -103,7 +113,7 @@ function App() {
                       )}
                       submitGuesses={submitGuesses}
                       selectedPlayer={selectedPlayer}
-                      loginId={user?.signInDetails?.loginId ?? ''}
+                      loginId={user?.signInDetails?.loginId ?? ""}
                     />
                   </>
                 )}
